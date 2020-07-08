d277fcbcd8c9142b6ed8dcb3f4d31e14d3aaa52b
Kostas Ntallas <kostasdallas2010@hotmail.com>
2020-07-08 21:05:22 +0300

    added axios and react hooks to perform async call

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import HelloWorldService from '../api/HelloWorldService';
 
 function Welcome(props) {
+
+  const [welcomeMessage, setWelcomeMessage] = useState('');
+
   const retrieveWelcomeMessage = () => {
-    HelloWorldService.executeHelloWorldService();
+    HelloWorldService.executeHelloWorldService()
+    .then( response => setWelcomeMessage(response.data) );
   }
 
   return (
@@ -12,6 +16,7 @@ function Welcome(props) {
       <h1>Welcome {props.match.params.name}, here are your <Link to="/todos">todos</Link></h1>
       <h2>Click here to get a customized Welcome Message</h2>
       <button className="btn btn-success" onClick={retrieveWelcomeMessage}>Get Welcome Message</button>
+      { welcomeMessage && <h3>{welcomeMessage}</h3> }
     </div>
   );
 }
