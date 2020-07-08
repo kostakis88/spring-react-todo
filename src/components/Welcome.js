359b2d63a0095e1e06af34acf58b74184159f547
Kostas Ntallas <kostasdallas2010@hotmail.com>
2020-07-08 20:26:46 +0300

    added axios and basic prep to display async message

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HelloWorldService from '../api/HelloWorldService';
 
 function Welcome(props) {
+  const retrieveWelcomeMessage = () => {
+    HelloWorldService.executeHelloWorldService();
+  }
+
   return (
     <div className="container">
       <h1>Welcome {props.match.params.name}, here are your <Link to="/todos">todos</Link></h1>
+      <h2>Click here to get a customized Welcome Message</h2>
+      <button className="btn btn-success" onClick={retrieveWelcomeMessage}>Get Welcome Message</button>
     </div>
   );
 }
