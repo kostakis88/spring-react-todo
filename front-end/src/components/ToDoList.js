2fae60dae4344a8fe204f16507c02a35856ecaa4
Kostas Ntallas <kostasdallas2010@hotmail.com>
2020-07-13 17:31:19 +0300

    added delete method

--- front-end/src/components/ToDoList.js
+++ front-end/src/components/ToDoList.js
@@ -4,16 +4,32 @@ import Authentication from './Authentication';
 
 class ToDoList extends Component {
   state = {
-    todos: []
+    todos: [],
+    message: null
   };
 
   componentDidMount() {
     let username = Authentication.getLoggedInUsername();
+    this.refreshTodos(username);
+  }
+
+  refreshTodos = (username) => {
     TodoDataService.retrieveAllTodos(username)
+    .then(response => {
+      this.setState({
+        todos: response.data
+      });
+    });
+  }
+
+  handleDelteClick = (id) => {
+    let username = Authentication.getLoggedInUsername();
+    TodoDataService.deleteTodo(username, id)
       .then(response => {
         this.setState({
-          todos: response.data
-        });
+          message: `Todo with id ${id} is successfully deleted!`
+        }); 
+        this.refreshTodos(username);
       });
   }
 
@@ -21,12 +37,14 @@ class ToDoList extends Component {
     return (
       <div className="container">
         <h1>ToDos</h1>
+        {this.state.message && <div className="alert alert-success">{this.state.message}</div>}
         <table className="table">
         <thead>
           <tr>
             <th>Description</th>
             <th>Due Date</th>
             <th>is it Completed?</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
@@ -35,6 +53,7 @@ class ToDoList extends Component {
               <td>{todo.description}</td>
               <td>{todo.targetDate.toString()}</td>
               <td>{todo.done.toString()}</td>
+              <td><button className="btn btn-warning" onClick={() => this.handleDelteClick(todo.id)}>Delete</button></td>
             </tr>
           )}
         </tbody>
--- restful-web-services/demo/src/main/java/com/kostas/rest/webservices/demo/todo/TodoResource.java
+++ restful-web-services/demo/src/main/java/com/kostas/rest/webservices/demo/todo/TodoResource.java
@@ -1,7 +1,9 @@
 package com.kostas.rest.webservices.demo.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +20,17 @@ public class TodoResource {
     @GetMapping("/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username) {
         return todoService.findAll();
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
+
+        Todo todo = todoService.deleteById(id);
+        if (todo!=null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+
     }
 }
