dbf0cc6f1dd9528dc62548be614b52db9d1444f6
Kostas Ntallas <kostasdallas2010@hotmail.com>
2020-07-13 20:04:41 +0300

    imported formik, moment and implemented basic form design

--- front-end/src/components/ToDo.js
+++ front-end/src/components/ToDo.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import moment from 'moment';
+import { Formik, Form, Field } from 'formik';
 
-function Todo(props) {
-  return (<div>Welcome to ToDo {props.match.params.id}</div>);
+class Todo extends Component {
+  
+  state = {
+    id: this.props.match.params.id,
+    description: 'Learn Forms',
+    targetDate: moment(new Date()).format('YYYY-MM-DD')
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Todo</h1>
+        <div className="container">
+          <Formik>
+            {(props) => (
+              <Form>
+                <fieldset className="form-group">
+                  <label>Description</label>
+                  <Field className="form-control" type="text" name="description"/>
+                </fieldset>
+                <fieldset className="form-group">
+                  <label>Target Date</label>
+                  <Field className="form-control" type="date" name="targetDate"/>
+                </fieldset>
+                <button className="btn btn-success" type="submit">Save</button>
+              </Form>
+              )}
+          </Formik>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Todo;
